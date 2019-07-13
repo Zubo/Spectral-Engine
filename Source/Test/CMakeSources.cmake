64569c2741c6920dfbf0795214a13767bb097caf
Ivo Zubović <ivo.zubovic@gmail.com>
2019-07-13 21:28:48 +0200

    Moved Test/Core/Utils/Math to Test/Core/Math

--- Source/Test/CMakeSources.cmake
+++ Source/Test/CMakeSources.cmake
@@ -2,19 +2,19 @@ set(SpectralEngine_Test_Main
     "main.cpp")
 
 set(SpectralEngine_Test_Core_Utility_Math_Source
-    "Core/Utility/Math/DegreeTest.cpp"
-    "Core/Utility/Math/LinearTransformationsTest.cpp"
-    "Core/Utility/Math/Matrix4x4Test.cpp"
-    "Core/Utility/Math/QuaternionTest.cpp"
-    "Core/Utility/Math/RadianTest.cpp"
-    "Core/Utility/Math/DegreeTest.cpp"
-    "Core/Utility/Math/Vector3Test.cpp"
-    "Core/Utility/Math/Vector2Test.cpp")
+    "Core/Math/DegreeTest.cpp"
+    "Core/Math/LinearTransformationsTest.cpp"
+    "Core/Math/Matrix4x4Test.cpp"
+    "Core/Math/QuaternionTest.cpp"
+    "Core/Math/RadianTest.cpp"
+    "Core/Math/DegreeTest.cpp"
+    "Core/Math/Vector3Test.cpp"
+    "Core/Math/Vector2Test.cpp")
 
 set(SpectralEngine_Test_Files
     "CMakeSources.cmake")
 
-source_group("Core\\Utility\\Math" FILES ${SpectralEngine_Test_Core_Utility_Math_Source})
+source_group("Core\\Math" FILES ${SpectralEngine_Test_Core_Utility_Math_Source})
 
 set(SpectralEngine_Test_SRC
     ${SpectralEngine_Test_Main}
