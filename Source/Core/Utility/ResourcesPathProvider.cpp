7d738cad3f1ea4ccad90ba310f3ec9b934a1e4c3
Ivo Zubović <ivo.zubovic@gmail.com>
2020-06-14 19:35:38 +0200

    Proper finding of Resources directory on MSVC compiled builds

--- Source/Core/Utility/ResourcesPathProvider.cpp
+++ Source/Core/Utility/ResourcesPathProvider.cpp
@@ -13,7 +13,12 @@ namespace sp {
 	}
 
 	void ResourcesPathProvider::initializePaths(SpString const & rootPath) {
+		// MSVC compiler puts executable into additional Debug/Release directory
+#ifdef _WIN32
+		resourcesDirectoryPath = rootPath + SpString{ "/../../Resources" };
+#else
 		resourcesDirectoryPath = rootPath + SpString{ "/../Resources" };
+#endif
 		shaderFilesDirectoryPath = resourcesDirectoryPath + SpString{ "/Shaders" };
 	}
 }
