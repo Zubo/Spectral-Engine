14442a3b9e2f8282bea2fc17ac7ec62992590de6
Ivo Zubović <ivo.zubovic@gmail.com>
2021-01-30 19:08:05 +0100

    PlatformIndependence project folder restructuring

--- Source/PlatformIndependence/CMakeSources.cmake
+++ Source/PlatformIndependence/CMakeSources.cmake
@@ -1,29 +1,29 @@
 set(SpectralEngine_PlatformIndependence_NoFilter_SRC
-    "SpWindow.cpp"
-    "Environment.cpp")
+    "src/SpWindow.cpp"
+    "src/Environment.cpp")
 
 set(SpectralEngine_PlatformIndependence_NoFilter_HEADER
-    "SpWindow.h"
-    "SpString.h"
-    "SpType.h"
-    "SpHash.h"
-    "Environment.h")
+    "include/PlatformIndependence/SpWindow.h"
+    "include/PlatformIndependence/SpString.h"
+    "include/PlatformIndependence/SpType.h"
+    "include/PlatformIndependence/SpHash.h"
+    "include/PlatformIndependence/Environment.h")
 
 set(SpectralEngine_PlatformIndependence_Input_SRC
-    "Input/Input.cpp"
-    "Input/KeyInputManager.cpp"
-    "Input/MouseInputManager.cpp")
+    "src/Input/Input.cpp"
+    "src/Input/KeyInputManager.cpp"
+    "src/Input/MouseInputManager.cpp")
 
 set(SpectralEngine_PlatformIndependence_Input_HEADER
-    "Input/Input.h"
-    "Input/KeyInputManager.h"
-    "Input/MouseInputManager.h"
-    "Input/KeyCode.h")
+    "include/PlatformIndependence/Input/Input.h"
+    "include/PlatformIndependence/Input/KeyInputManager.h"
+    "include/PlatformIndependence/Input/MouseInputManager.h"
+    "include/PlatformIndependence/Input/KeyCode.h")
 
-source_group("Source Files" FILES ${SpectralEngine_PlatformIndependence_NoFilter_SRC})
-source_group("Header Files" FILES ${SpectralEngine_PlatformIndependence_NoFilter_HEADER})
-source_group("Source Files\\Input" FILES ${SpectralEngine_PlatformIndependence_Input_SRC})
-source_group("Header Files\\Input" FILES ${SpectralEngine_PlatformIndependence_Input_HEADER})
+source_group("src" FILES ${SpectralEngine_PlatformIndependence_NoFilter_SRC})
+source_group("include\\PlatformIndependence" FILES ${SpectralEngine_PlatformIndependence_NoFilter_HEADER})
+source_group("src\\Input" FILES ${SpectralEngine_PlatformIndependence_Input_SRC})
+source_group("include\\PlatformIndependence\\Input" FILES ${SpectralEngine_PlatformIndependence_Input_HEADER})
 
 set(SpectralEngine_PlatformIndependence_SRC
     ${SpectralEngine_PlatformIndependence_NoFilter_SRC}
